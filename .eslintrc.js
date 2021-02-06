1a34e0d8128b89e5ecdb0c3fc4b55a80db99ad24
borsTiHD <bastian.j@gmx.net>
2021-02-06 17:31:26 +0100

    ESLint >> Linting Rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,46 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    extends: [
+        '@nuxtjs',
+        'prettier',
+        'prettier/vue',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended',
+    ],
+    plugins: ['prettier'],
+    // add your custom rules here
+    rules: {
+        indent: [
+            'warn',
+            4,
+            {
+                outerIIFEBody: 1,
+                ignoredNodes: ['TemplateLiteral'],
+                SwitchCase: 1,
+            },
+        ], // 4 spaces – for indentation
+        'template-curly-spacing': 'off',
+        'max-len': ['off', { code: 120 }],
+        'no-console': 'off',
+        'arrow-parens': ['error', 'always'],
+        'space-before-function-paren': ['error', 'never'],
+        curly: ['error', 'multi-line'],
+        'import/no-extraneous-dependencies': 'off',
+        'require-await': 0,
+
+        'global-require': 0,
+        'import/no-unresolved': 0,
+        'import/newline-after-import': 0,
+        'no-underscore-dangle': 0,
+
+        'vue/max-attributes-per-line': 'off',
+        'vue/singleline-html-element-content-newline': 0,
+    },
 }
