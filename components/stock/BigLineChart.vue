5f5e612867923fc6f3cfb4e2ba4dc97866f74b00
borsTiHD <bastian.j@gmx.net>
2021-02-13 10:03:51 +0100

    Stock >> Jahres Chart in separate Component verbannt.

---
+++ components/stock/BigLineChart.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-card class="flex d-flex flex-column">
+        <v-card-text>
+            <v-progress-linear v-if="loadingData" indeterminate color="white" />
+            <stock-line-chart
+                v-else
+                :chart-data="stockValue"
+                :chart-labels="stockLabels"
+                :line-color="chartColor.line"
+                :bg-color="chartColor.bg"
+            />
+        </v-card-text>
+
+        <v-card-text>
+            <div class="display-1 font-weight-thin">Last Year Performance</div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import moment from 'moment'
+import StockLineChart from '~/components/charts/StockLineChart.vue'
+
+export default {
+    components: {
+        StockLineChart
+    },
+    props: {
+        stockData: {
+            type: [Array, Object],
+            required: true,
+            default: () => {
+                return {}
+            }
+        },
+        loadingData: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    computed: {
+        stockValue() {
+            const data = this.stockData?.eod
+            if (!Array.isArray(data)) return []
+            return data.map((v) => {
+                // const midpoint = (v.high + v.low) / 2 // Errechnet den Mittelpunkt vom Höchst-/Tiefstwert
+                return v.close
+            })
+        },
+        stockLabels() {
+            const data = this.stockData?.eod
+            if (!Array.isArray(data)) return []
+            return data.map((v) => {
+                // return v.date
+                return moment(v.date).format('YYYY.MM.DD')
+            })
+        },
+        chartColor() {
+            const data = this.stockData?.eod
+            if (!Array.isArray(data)) return { line: '#ffffffb3', bg: '#FFFFFF1A' }
+
+            // Ermittelt Data
+            const length = data.length
+            const firstData = data[0]
+            const lastData = data[length-1]
+
+            // Vergleicht 'Opening' mit 'Closing' Data
+            if (firstData.open < lastData.close) {
+                return { line: '#03fc62', bg: '#03fc621A' } // 'bg' ist 10% transparency
+            } else {
+                return { line: '#cc2929', bg: '#cc29291A' } // 'bg' ist 10% transparency
+            }
+        }
+    }
+}
+</script>
--- pages/stock.vue
+++ pages/stock.vue
@@ -88,26 +88,10 @@
             </v-card>
         </v-col>
         <v-col cols="12" class="d-flex flex-column">
-            <v-card class="flex d-flex flex-column">
-                <v-card-text>
-                    <v-progress-linear
-                        v-if="loadingData"
-                        indeterminate
-                        color="white"
-                    />
-                    <stock-line-chart
-                        v-else
-                        :chart-data="stockValue"
-                        :chart-labels="stockLabels"
-                        :line-color="chartColor.line"
-                        :bg-color="chartColor.bg"
-                    />
-                </v-card-text>
-
-                <v-card-text>
-                    <div class="display-1 font-weight-thin">Last Year</div>
-                </v-card-text>
-            </v-card>
+            <big-line-chart
+                :loading-data="loadingData"
+                :stock-data="stockData"
+            />
         </v-col>
     </v-row>
 </template>
@@ -116,11 +100,11 @@
 import { mapGetters } from 'vuex'
 import moment from 'moment'
 
-import StockLineChart from '~/components/charts/StockLineChart.vue'
+import BigLineChart from '~/components/stock/BigLineChart.vue'
 
 export default {
     components: {
-        StockLineChart
+        BigLineChart
     },
     data() {
         return {
@@ -144,38 +128,6 @@ export default {
                 return { name: '', symbol: '' }
             }
             return this.getCurrentStock
-        },
-        stockValue() {
-            const data = this.stockData?.eod
-            if (!Array.isArray(data)) return []
-            return data.map((v) => {
-                // const midpoint = (v.high + v.low) / 2 // Errechnet den Mittelpunkt vom Höchst-/Tiefstwert
-                return v.close
-            })
-        },
-        stockLabels() {
-            const data = this.stockData?.eod
-            if (!Array.isArray(data)) return []
-            return data.map((v) => {
-                // return v.date
-                return moment(v.date).format('YYYY.MM.DD')
-            })
-        },
-        chartColor() {
-            const data = this.stockData?.eod
-            if (!Array.isArray(data)) return { line: '#ffffffb3', bg: '#FFFFFF1A' }
-
-            // Ermittelt Data
-            const length = data.length
-            const firstData = data[0]
-            const lastData = data[length-1]
-
-            // Vergleicht 'Opening' mit 'Closing' Data
-            if (firstData.open < lastData.close) {
-                return { line: '#03fc62', bg: '#03fc621A' } // 'bg' ist 10% transparency
-            } else {
-                return { line: '#cc2929', bg: '#cc29291A' } // 'bg' ist 10% transparency
-            }
         }
     },
     watch: {
