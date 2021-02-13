edcaa8e96b8f41c60cd8c235553bb4da076c6101
borsTiHD <bastian.j@gmx.net>
2021-02-13 12:24:46 +0100

    Component >> Eigene Comp für die Prozentanzeige zweier Werte.

---
+++ components/display/PercentageSheet.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-sheet
+        rounded="lg"
+        class="d-flex align-content-center px-1 font-weight-black subtitle-1"
+        :color="color() + ' lighten-1'"
+    >
+        <v-icon :color="color() + ' darken-3'" class="align-self-center">
+            {{ icon() }}
+        </v-icon>
+        <span
+            :class="color() + '--text text--darken-3'"
+            class="align-self-center"
+        >
+            {{ percentageIncrease() }} %
+        </span>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    props: {
+        openingValue: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        closingValue: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+    },
+    methods: {
+        color() {
+            if (this.openingValue < this.closingValue) {
+                return 'green'
+            }
+            return 'red'
+        },
+        icon() {
+            if (this.openingValue < this.closingValue) {
+                return 'mdi-menu-up'
+            }
+            return 'mdi-menu-down'
+        },
+        percentageIncrease() {
+            if (this.openingValue === 0 && this.closingValue === 0) return 0
+
+            // Absolute Erhöhung in Prozent bestimmen
+            const diff = this.closingValue - this.openingValue
+            const percentage = Math.round(((diff / this.closingValue) * 100) * 100) / 100 // Runden auf zwei Nachkommastellen: 'Math.round(217.4325 * 100) / 100'
+
+            /*
+            console.log('Closing:', this.closingValue)
+            console.log('Opening:', this.openingValue)
+            console.log('Diff:', diff)
+            console.log('Percentage:', percentage)
+            */
+
+            return percentage
+        }
+    }
+}
+</script>
