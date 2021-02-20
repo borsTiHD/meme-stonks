1d832fe828bbff1f9b0390661a0632f69b4bb876
borsTiHD <bastian.j@gmx.net>
2021-02-20 11:28:40 +0100

    Middleware >> API Check angepasst.

--- middleware/checkApiKey.js
+++ middleware/checkApiKey.js
@@ -1,10 +1,10 @@
 export default function(context) {
     // Wenn der API Key nicht vorhanden ist, wird der User zur Welcome Page zurück geleitet
     const apiToken = context.store.getters.getApiToken
-    if (apiToken) {
-        console.log('[Middleware] >> API Key available')
-    } else {
+    if (!apiToken || apiToken === 'undefined' || apiToken === 'null' || apiToken === '') {
         console.log('[Middleware] >> API Key is NOT available:', apiToken)
         return context.redirect('/')
+    } else {
+        console.log('[Middleware] >> API Key available')
     }
 }
