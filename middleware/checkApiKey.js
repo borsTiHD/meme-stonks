80eb02d6b2e8fecd8126c0cae9768ce83c1e02ba
borsTiHD <bastian.j@gmx.net>
2021-02-20 12:01:05 +0100

    Middleware >> Schickt User zu Settings.

--- middleware/checkApiKey.js
+++ middleware/checkApiKey.js
@@ -1,9 +1,9 @@
 export default function(context) {
-    // Wenn der API Key nicht vorhanden ist, wird der User zur Welcome Page zurück geleitet
+    // Wenn der API Key nicht vorhanden ist, wird der User zur Settings Page geleitet
     const apiToken = context.store.getters.getApiToken
     if (!apiToken || apiToken === 'undefined' || apiToken === 'null' || apiToken === '') {
         console.log('[Middleware] >> API Key is NOT available:', apiToken)
-        return context.redirect('/')
+        return context.redirect('/settings')
     } else {
         console.log('[Middleware] >> API Key available')
     }
