c3b56106bbe09b2137615663e8564db9f261f6f2
borsTiHD <bastian.j@gmx.net>
2021-02-21 12:18:17 +0100

    Footer >> Anzeige erweitert. Unteranderem Repo Link hinzugefügt.

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,15 +1,33 @@
 <template>
-    <v-footer absolute app>
-        <span>&copy; {{ date }}</span>
+    <v-footer absolute app class="d-flex justify-center">
+        <span class="align-self-center font-weight-thin">&copy; {{ date }} All Rights Reserved</span>
+        <span class="align-self-center"><v-icon color="pink">mdi-circle-small</v-icon></span>
+        <span class="align-self-center font-weight-thin"><v-icon>mdi-code-tags</v-icon> with <v-icon color="pink">mdi-heart</v-icon> in Germany</span>
+        <span class="align-self-center"><v-icon color="pink">mdi-circle-small</v-icon></span>
+        <span class="align-self-center font-weight-thin"><a class="link" :href="repoUrl" target="_blank">Source at <v-icon>mdi-github</v-icon></a></span>
     </v-footer>
 </template>
 
 <script>
+import pkg from './../../package.json'
+
 export default {
     data() {
         return {
             date: `${new Date().getFullYear()}`
         }
+    },
+    computed: {
+        repoUrl() {
+            return pkg.repository.url
+        }
     }
 }
 </script>
+
+<style scoped>
+    .link {
+        color: white;
+        text-decoration: none;
+    }
+</style>
