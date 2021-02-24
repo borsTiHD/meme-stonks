362e95034dead920101eef812e4f5e5b86481f9c
borsTiHD <bastian.j@gmx.net>
2021-02-24 10:56:24 +0100

    iDB >> Erste DB Struktur für User Settings.

--- plugins/indexeddb.js
+++ plugins/indexeddb.js
@@ -5,11 +5,16 @@ import { openDB } from 'idb'
  */
 class IndexedDb {
     constructor(app) {
-        // Nuxt App Instance
+        // Nuxt App Instanz
         this.$app = app
 
-        // DB Instance
-        this.db1 = this.init()
+        // Error Messages
+        this.err = {
+            DB_VERSION_UNKNOWN: 'Unknown DB Version'
+        }
+
+        // iDB Instanzen
+        this.idb = this.init()
     }
 
     /**
@@ -18,7 +23,69 @@ class IndexedDb {
      * @return  {object}    -> Returned Datenbank Verbindungen
      */
     async init() {
-        return await openDB('db1', 1)
+        const idb = {
+            userSettings: await this.initUserSettings(1)
+        }
+        return idb
+    }
+
+    /**
+     * initUserSettings() - Initiiert UserSettings
+     * @access  private
+     * @return  {object}    -> Returned Datenbank
+     */
+    async initUserSettings(version) {
+        return await openDB('userSettings', version, {
+            upgrade: async(db, oldVersion, newVersion, transaction) => {
+                switch (oldVersion) {
+                    case 0:
+                        upgradeDB3fromV0toV1()
+                        break // break entfernen wenn weitere Upgrades hinzukommen
+                        // falls through
+                    /*
+                    case 1:
+                        upgradeDB3fromV1toV2()
+                        // falls through
+                    case 2:
+                        await upgradeDB3fromV2toV3()
+                        break
+                    */
+                    default:
+                        console.error(this.err.DB_VERSION_UNKNOWN)
+                }
+
+                // Erstellt Datenbank Struktur
+                function upgradeDB3fromV0toV1() {
+                    // User Einstellungen
+                    db.createObjectStore('preference')
+                    transaction.objectStore('preference').put(true, 'darkMode')
+
+                    // User Tokens
+                    db.createObjectStore('tokens')
+                    transaction.objectStore('tokens').put(null, 'stockApiToken')
+                    transaction.objectStore('tokens').put(null, 'stockApiTokenPremium')
+                }
+
+                /*
+                function upgradeDB3fromV1toV2() {
+                    db.createObjectStore('userPreference')
+                    transaction.objectStore('userPreference').add(false, 'useDarkMode')
+                    transaction.objectStore('userPreference').add(25, 'resultsPerPage')
+                }
+                async function upgradeDB3fromV2toV3() {
+                    const store = transaction.objectStore('userPreference')
+                    store.put('English', 'language')
+                    store.delete('resultsPerPage')
+                    let colorTheme = 'automatic'
+                    const useDarkMode = await store.get('useDarkMode')
+                    if (oldVersion === 2 && useDarkMode === false) colorTheme = 'light'
+                    if (oldVersion === 2 && useDarkMode === true) colorTheme = 'dark'
+                    store.put(colorTheme, 'colorTheme')
+                    store.delete('useDarkMode')
+                }
+                */
+            }
+        })
     }
 }
 
