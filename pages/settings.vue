9b674393e63cb876ab237d2ce44a8f55a3291fe9
borsTiHD <bastian.j@gmx.net>
2021-02-27 10:41:46 +0100

    Settings >> Vorstellunge Free / Premium mit Partnerlink.

--- pages/settings.vue
+++ pages/settings.vue
@@ -3,13 +3,13 @@
         <v-row justify="center" align="center">
             <v-col cols="12" sm="8" md="6">
                 <v-card>
-                    <v-card-title class="headline">Set Api Tokens</v-card-title>
+                    <v-card-title class="headline">Set Api Token</v-card-title>
                     <v-card-text>
                         <v-row>
-                            <v-col cols="12" md="4">
+                            <v-col cols="12" md="3">
                                 <v-subheader>Marketstack.com</v-subheader>
                             </v-col>
-                            <v-col cols="12" md="6">
+                            <v-col>
                                 <v-text-field
                                     v-model="stockApiToken"
                                     label="set api token for stock requests"
@@ -29,8 +29,82 @@
                                             />
                                         </span>
                                     </template>
-                                    <span>Premium Key: Provides Intraday market data & real-time updates for 125,000+ tickers.</span>
+                                    <span>Differs between free/premium key.</span>
                                 </v-tooltip>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col col="12">
+                                <v-alert
+                                    icon="mdi-shield-lock-outline"
+                                    prominent
+                                    text
+                                    type="info"
+                                >
+                                    <span>
+                                        All stock calls are made via this. The api key will be stored on your local device (indexxeddb).
+                                    </span>
+                                    <span>
+                                        You can set up a free, or premium key via the following link. The link is an affiliate program, I get a commission if a purchase is made. The prices remain the same.
+                                    </span>
+
+                                    <v-divider
+                                        class="my-4 info"
+                                        style="opacity: 0.22"
+                                    />
+
+                                    <v-tooltip bottom>
+                                        <template #activator="{ on }">
+                                            <v-btn
+                                                color="blue-grey"
+                                                class="ma-2 white--text"
+                                                :href="stockApiAffiliateUrl"
+                                                target="_blank"
+                                                v-on="on"
+                                            >
+                                                Get a Key
+                                            </v-btn>
+                                        </template>
+                                        <span>Sends you to marketstack.com</span>
+                                    </v-tooltip>
+                                </v-alert>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col col="6">
+                                <v-alert
+                                    color="blue-grey"
+                                    dark
+                                    dense
+                                    icon="mdi-school"
+                                    prominent
+                                    text
+                                >
+                                    <span>Free key:</span>
+                                    <ul>
+                                        <li v-for="(plan, index) in stockApiPlanDescription.free" :key="index">
+                                            {{ plan }}
+                                        </li>
+                                    </ul>
+                                </v-alert>
+                            </v-col>
+
+                            <v-col col="6">
+                                <v-alert
+                                    color="success"
+                                    dark
+                                    dense
+                                    icon="mdi-school"
+                                    prominent
+                                    text
+                                >
+                                    <span>Premium key (in addition):</span>
+                                    <ul>
+                                        <li v-for="(plan, index) in stockApiPlanDescription.premium" :key="index">
+                                            {{ plan }}
+                                        </li>
+                                    </ul>
+                                </v-alert>
                             </v-col>
                         </v-row>
                     </v-card-text>
@@ -39,7 +113,7 @@
 
                     <v-card-actions>
                         <v-spacer />
-                        <v-btn color="primary" text @click="setTokens">Set Tokens</v-btn>
+                        <v-btn color="primary" text @click="setTokens">Set Token</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -115,6 +189,19 @@ export default {
         return {
             stockApiToken: '',
             stockApiPremiumCheckbox: false,
+            stockApiAffiliateUrl: 'https://marketstack.com?fpr=meme-stonks',
+            stockApiPlanDescription: {
+                free: [
+                    'limited to 1,000 Requests / mo',
+                    '1 Year History',
+                    'End-of-Day Data'
+                ],
+                premium: [
+                    '10,000 Requests / mo',
+                    'Intraday Data',
+                    'HTTPS Encryption'
+                ]
+            },
             exchange: '',
             snackbar: {
                 show: false,
